aba74f250dc2ca918255d0550490b9ddcc0f8638
sriramjhansi <jhansisriram22@gmail.com>
2023-03-23 15:16:05 +0530

    Waits code has return

---
+++ src/test/java/Waits.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Waits {
+	
+	public static void main(String[] args) {
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(co);
+		driver.get("https://www.facebook.com");
+	}
+
+}
